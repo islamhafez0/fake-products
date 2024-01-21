48cccd965a7bb2d901674ffefbefb81a5dee643a
islam hafez <aboeshaeslam2@gmail.com>
2024-01-21 21:56:29 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import ProductCard from './components/ProductCard'
